13309899715096ba1462e3a4ac68ed3c8a85c05c
Nishkal <127597678+Nishkal08@users.noreply.github.com>
2025-09-25 19:36:08 +0530

    bug fix

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,20 +1,27 @@
 const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    auth: {
-        user: "apikey",
-        pass: process.env.SENDGRID_API_KEY,
-    },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async (to, subject, text, name) => {
     try {
-        await transporter.sendMail({
-            from: `"Uber" <nishkal0810@gmail.com>`,
+        //         await transporter.sendMail({
+        //             from: `"Uber" <nishkal0810@gmail.com>`,
+        //             to,
+        //             subject,
+        //             html: `
+        //     <div style="font-family: Arial, sans-serif; text-align: center;">
+        //       <img src="https://blog.uber-cdn.com/cdn-cgi/image/width=2160,quality=80,onerror=redirect,format=auto/wp-content/uploads/2021/10/One-Uber-2021.png" />
+        //       <h2>Welcome, ${name} 🚀</h2>
+        //       <p>${text}<b>Uber Clone</b>. We're glad to have you on board!</p>
+        //     </div>
+        //   `,
+        // });
+        const msg = {
             to,
+            from: 'nishkal0810@gmail.com', 
             subject,
+            text: `Hello ${name},\n\n${text}`,
             html: `
     <div style="font-family: Arial, sans-serif; text-align: center;">
       <img src="https://blog.uber-cdn.com/cdn-cgi/image/width=2160,quality=80,onerror=redirect,format=auto/wp-content/uploads/2021/10/One-Uber-2021.png" />
@@ -22,7 +29,9 @@ const sendMail = async (to, subject, text, name) => {
       <p>${text}<b>Uber Clone</b>. We're glad to have you on board!</p>
     </div>
   `,
-        });
+        };
+        await sgMail.send(msg);
+
         console.log("✅ Email sent successfully");
     } catch (error) {
         console.error("❌ Error sending email:", error);
